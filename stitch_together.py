ccfede56379ffaa7fc36baa402c0e617df5ad5a6
francesco-vaccari <francesco.vaccari@studenti.unitn.it>
2024-08-12 15:13:30 +0200

    Update stitch_together.py

--- stitch_together.py
+++ stitch_together.py
@@ -21,70 +21,29 @@ params2 = {'r': 0, 's': 100, 'x': 0, 'y': 0}
 params3 = {'r': 0, 's': 100, 'x': 0, 'y': 0}
 params = [params1, params2, params3]
 focus = 0
-order = 1
+top_merging_line = int(top_video.get(cv2.CAP_PROP_FRAME_HEIGHT) + pad // 2)
+bottom_merging_line = int(top_video.get(cv2.CAP_PROP_FRAME_HEIGHT) + center_video.get(cv2.CAP_PROP_FRAME_HEIGHT) + pad // 2)
+show_merging_lines = True
 
 cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
 
-def draw_on_canvas(canvas, start_x, start_y, end_x, end_y, frame):
-    for i in range(start_y, end_y):
-        for j in range(start_x, end_x):
-            if np.all(canvas[i][j] == 0):
-                canvas[i][j] = frame[i - start_y][j - start_x]
-    return canvas
+def copy_frames(top, top_start, center, center_start, bottom, bottom_start):
+    top_canvas = np.zeros((top.shape[0] + center.shape[0] + bottom.shape[0] + pad, top.shape[1] + pad, 3), dtype=np.uint8)
+    center_canvas = np.zeros((top.shape[0] + center.shape[0] + bottom.shape[0] + pad, top.shape[1] + pad, 3), dtype=np.uint8)
+    bottom_canvas = np.zeros((top.shape[0] + center.shape[0] + bottom.shape[0] + pad, top.shape[1] + pad, 3), dtype=np.uint8)
 
-def copy_frames2(canvas, top, top_start, center, center_start, bottom, bottom_start):
-    if order == 1 or order == 2:
-        canvas = draw_on_canvas(canvas, top_start[0], top_start[1], top_start[0] + top.shape[1], top_start[1] + top.shape[0], top)
-        if order == 1:
-            canvas = draw_on_canvas(canvas, center_start[0], center_start[1], center_start[0] + center.shape[1], center_start[1] + center.shape[0], center)
-            canvas = draw_on_canvas(canvas, bottom_start[0], bottom_start[1], bottom_start[0] + bottom.shape[1], bottom_start[1] + bottom.shape[0], bottom)
-        if order == 2:
-            canvas = draw_on_canvas(canvas, bottom_start[0], bottom_start[1], bottom_start[0] + bottom.shape[1], bottom_start[1] + bottom.shape[0], bottom)
-            canvas = draw_on_canvas(canvas, center_start[0], center_start[1], center_start[0] + center.shape[1], center_start[1] + center.shape[0], center)
-    if order == 3 or order == 4:
-        canvas = draw_on_canvas(canvas, center_start[0], center_start[1], center_start[0] + center.shape[1], center_start[1] + center.shape[0], center)
-        if order == 3:
-            canvas = draw_on_canvas(canvas, top_start[0], top_start[1], top_start[0] + top.shape[1], top_start[1] + top.shape[0], top)
-            canvas = draw_on_canvas(canvas, bottom_start[0], bottom_start[1], bottom_start[0] + bottom.shape[1], bottom_start[1] + bottom.shape[0], bottom)
-        if order == 4:
-            canvas = draw_on_canvas(canvas, bottom_start[0], bottom_start[1], bottom_start[0] + bottom.shape[1], bottom_start[1] + bottom.shape[0], bottom)
-            canvas = draw_on_canvas(canvas, top_start[0], top_start[1], top_start[0] + top.shape[1], top_start[1] + top.shape[0], top)
-    if order == 5 or order == 6:
-        canvas = draw_on_canvas(canvas, bottom_start[0], bottom_start[1], bottom_start[0] + bottom.shape[1], bottom_start[1] + bottom.shape[0], bottom)
-        if order == 5:
-            canvas = draw_on_canvas(canvas, top_start[0], top_start[1], top_start[0] + top.shape[1], top_start[1] + top.shape[0], top)
-            canvas = draw_on_canvas(canvas, center_start[0], center_start[1], center_start[0] + center.shape[1], center_start[1] + center.shape[0], center)
-        if order == 6:
-            canvas = draw_on_canvas(canvas, center_start[0], center_start[1], center_start[0] + center.shape[1], center_start[1] + center.shape[0], center)
-            canvas = draw_on_canvas(canvas, top_start[0], top_start[1], top_start[0] + top.shape[1], top_start[1] + top.shape[0], top)
 
-    return canvas
+    top_canvas[top_start[1]:top_start[1] + top.shape[0], top_start[0]:top_start[0] + top.shape[1]] = top
+    top_canvas[top_merging_line:, :] = 0
 
-def copy_frames(canvas, top, top_start, center, center_start, bottom, bottom_start):
-    if order == 1 or order == 2:
-        canvas[top_start[1]:top_start[1] + top.shape[0], top_start[0]:top_start[0] + top.shape[1]] = top
-        if order == 1:
-            canvas[center_start[1]:center_start[1] + center.shape[0], center_start[0]:center_start[0] + center.shape[1]] = center
-            canvas[bottom_start[1]:bottom_start[1] + bottom.shape[0], bottom_start[0]:bottom_start[0] + bottom.shape[1]] = bottom
-        if order == 2:
-            canvas[bottom_start[1]:bottom_start[1] + bottom.shape[0], bottom_start[0]:bottom_start[0] + bottom.shape[1]] = bottom
-            canvas[center_start[1]:center_start[1] + center.shape[0], center_start[0]:center_start[0] + center.shape[1]] = center
-    if order == 3 or order == 4:
-        canvas[center_start[1]:center_start[1] + center.shape[0], center_start[0]:center_start[0] + center.shape[1]] = center
-        if order == 3:
-            canvas[top_start[1]:top_start[1] + top.shape[0], top_start[0]:top_start[0] + top.shape[1]] = top
-            canvas[bottom_start[1]:bottom_start[1] + bottom.shape[0], bottom_start[0]:bottom_start[0] + bottom.shape[1]] = bottom
-        if order == 4:
-            canvas[bottom_start[1]:bottom_start[1] + bottom.shape[0], bottom_start[0]:bottom_start[0] + bottom.shape[1]] = bottom
-            canvas[top_start[1]:top_start[1] + top.shape[0], top_start[0]:top_start[0] + top.shape[1]] = top
-    if order == 5 or order == 6:
-        canvas[bottom_start[1]:bottom_start[1] + bottom.shape[0], bottom_start[0]:bottom_start[0] + bottom.shape[1]] = bottom
-        if order == 5:
-            canvas[top_start[1]:top_start[1] + top.shape[0], top_start[0]:top_start[0] + top.shape[1]] = top
-            canvas[center_start[1]:center_start[1] + center.shape[0], center_start[0]:center_start[0] + center.shape[1]] = center
-        if order == 6:
-            canvas[center_start[1]:center_start[1] + center.shape[0], center_start[0]:center_start[0] + center.shape[1]] = center
-            canvas[top_start[1]:top_start[1] + top.shape[0], top_start[0]:top_start[0] + top.shape[1]] = top
+    center_canvas[center_start[1]:center_start[1] + center.shape[0], center_start[0]:center_start[0] + center.shape[1]] = center
+    center_canvas[:top_merging_line, :] = 0
+    center_canvas[bottom_merging_line:, :] = 0
+
+    bottom_canvas[bottom_start[1]:bottom_start[1] + bottom.shape[0], bottom_start[0]:bottom_start[0] + bottom.shape[1]] = bottom
+    bottom_canvas[:bottom_merging_line, :] = 0
+
+    canvas = top_canvas + center_canvas + bottom_canvas
 
     return canvas
 
@@ -97,8 +56,6 @@ while top_video.isOpened():
         break
 
     top = cv2.rotate(top, cv2.ROTATE_180)
-
-    canvas = np.zeros((top.shape[0] + center.shape[0] + bottom.shape[0] + pad, top.shape[1] + pad, 3), dtype=np.uint8)
 
     top_start = (0 + pad // 2, 0 + pad // 2)
     center_start = (0 + pad // 2, top.shape[0] + pad // 2)
@@ -133,10 +90,18 @@ while top_video.isOpened():
     center = cv2.warpAffine(center, cv2.getRotationMatrix2D((center.shape[1] / 2, center.shape[0] / 2), params[1]['r'], 1), (center.shape[1], center.shape[0]))
     bottom = cv2.warpAffine(bottom, cv2.getRotationMatrix2D((bottom.shape[1] / 2, bottom.shape[0] / 2), params[2]['r'], 1), (bottom.shape[1], bottom.shape[0]))
 
-    canvas = copy_frames(canvas, top, top_start, center, center_start, bottom, bottom_start)
-    # canvas = copy_frames2(canvas, top, top_start, center, center_start, bottom, bottom_start)
+    canvas = copy_frames(top, top_start, center, center_start, bottom, bottom_start)
+
+    if show_merging_lines:
+        cv2.line(canvas, (0, top_merging_line), (canvas.shape[1], top_merging_line), (0, 255, 0), 5)
+        cv2.line(canvas, (0, bottom_merging_line), (canvas.shape[1], bottom_merging_line), (0, 255, 0), 5)
 
     cv2.imshow('Image', canvas)
+
+    if top_video.get(cv2.CAP_PROP_POS_FRAMES) == top_video.get(cv2.CAP_PROP_FRAME_COUNT):
+        top_video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        center_video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        bottom_video.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
     key = cv2.waitKey(1) & 0xFF
 
@@ -164,27 +129,24 @@ while top_video.isOpened():
     if key == ord('x'):
         # scale down
         params[focus]['s'] -= 0.2
-    if key == ord('l'):
+    if key == ord('f'):
         # switch focus to next video
         focus = (focus + 1) % 3
-    if key == ord('1'):
-        print('Order of drawing: top, center, bottom')
-        order = 1
-    if key == ord('2'):
-        print('Order of drawing: top, bottom, center')
-        order = 2
-    if key == ord('3'):
-        print('Order of drawing: center, top, bottom')
-        order = 3
-    if key == ord('4'):
-        print('Order of drawing: center, bottom, top')
-        order = 4
-    if key == ord('5'):
-        print('Order of drawing: bottom, top, center')
-        order = 5
-    if key == ord('6'):
-        print('Order of drawing: bottom, center, top')
-        order = 6
+    if key == ord('u'):
+        # move up top merging line
+        top_merging_line -= 1
+    if key == ord('j'):
+        # move down top merging line
+        top_merging_line += 1
+    if key == ord('i'):
+        # move up bottom merging line
+        bottom_merging_line -= 1
+    if key == ord('k'):
+        # move down bottom merging line
+        bottom_merging_line += 1
+    if key == ord('m'):
+        # show/hide merging lines
+        show_merging_lines = not show_merging_lines
     
     if key == ord(' '):
         break
@@ -209,16 +171,14 @@ out = cv2.VideoWriter(save_video, cv2.VideoWriter_fourcc(*'mp4v'), top_video.get
 progress_bar = tqdm.tqdm(total=int(top_video.get(cv2.CAP_PROP_FRAME_COUNT)))
 
 while top_video.isOpened():
-    top_ret, top = top_video.read()
-    center_ret, center = center_video.read()
-    bottom_ret, bottom = bottom_video.read()
+    ret1, top = top_video.read()
+    ret2, center = center_video.read()
+    ret3, bottom = bottom_video.read()
 
-    if not top_ret or not center_ret or not bottom_ret:
+    if not ret1 or not ret2 or not ret3:
         break
 
     top = cv2.rotate(top, cv2.ROTATE_180)
-
-    canvas = np.zeros((top.shape[0] + center.shape[0] + bottom.shape[0] + pad, top.shape[1] + pad, 3), dtype=np.uint8)
 
     top_start = (0 + pad // 2, 0 + pad // 2)
     center_start = (0 + pad // 2, top.shape[0] + pad // 2)
@@ -253,9 +213,15 @@ while top_video.isOpened():
     center = cv2.warpAffine(center, cv2.getRotationMatrix2D((center.shape[1] / 2, center.shape[0] / 2), params[1]['r'], 1), (center.shape[1], center.shape[0]))
     bottom = cv2.warpAffine(bottom, cv2.getRotationMatrix2D((bottom.shape[1] / 2, bottom.shape[0] / 2), params[2]['r'], 1), (bottom.shape[1], bottom.shape[0]))
 
-    # canvas = copy_frames(canvas, top, top_start, center, center_start, bottom, bottom_start)
-    canvas = copy_frames2(canvas, top, top_start, center, center_start, bottom, bottom_start)
-
+    canvas = copy_frames(top, top_start, center, center_start, bottom, bottom_start)
 
     out.write(canvas)
     progress_bar.update(1)
+
+top_video.release()
+center_video.release()
+bottom_video.release()
+out.release()
+
+progress_bar.close()
+cv2.destroyAllWindows()
