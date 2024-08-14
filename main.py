416015c3cae575bf5d77ec1225158aaa65497edc
francesco-vaccari <francesco.vaccari@studenti.unitn.it>
2024-08-14 11:32:50 +0200

    Added detection with yolo (still testing)

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import cv2
 from Detection import FrameDifferencing, BackgroundSubtractor, AdaptiveBackgroundSubtractor
+from yolov5_test import YOLOv5
+from yolov8_test import YOLOv8
+from PIL import Image
 
 FD = FrameDifferencing(threshold=50)
 BGSUB = BackgroundSubtractor(bg_path='background_image.jpg', threshold=50)
@@ -7,8 +10,9 @@ ABGSUB = AdaptiveBackgroundSubtractor(bg_path='background_image.jpg', alpha=0.01
 KNN = cv2.createBackgroundSubtractorKNN()
 MOG2 = cv2.createBackgroundSubtractorMOG2(detectShadows=False, varThreshold=50)
 
-detector = MOG2
-
+# detector = MOG2
+# yolo = YOLOv8()
+yolo = YOLOv5()
 
 
 
@@ -17,12 +21,10 @@ cap = cv2.VideoCapture('videos/refined.mp4')
 
 def get_bounding_boxes(mask):
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
     boxes = []
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
         boxes.append((x, y, x + w, y + h))
-    
     return boxes
 
 while cap.isOpened():
@@ -30,18 +32,26 @@ while cap.isOpened():
     if not ret:
         break
     
-    mask = detector.apply(frame)
+    # mask = detector.apply(frame)
 
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=18)
+    # mask = cv2.erode(mask, None, iterations=2)
+    # mask = cv2.dilate(mask, None, iterations=18)
 
-    boxes = get_bounding_boxes(mask)
+    # boxes = get_bounding_boxes(mask)
 
-    for box in boxes:
+    # for box in boxes:
+    #     x1, y1, x2, y2 = box
+    #     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    # cv2.imshow('Mask', mask)
+
+
+    bboxes = yolo.predict(frame)
+
+    for box in bboxes:
         x1, y1, x2, y2 = box
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    cv2.imshow('Mask', mask)
     cv2.imshow('Frame', frame)
     
     key = cv2.waitKey(1) & 0xFF
