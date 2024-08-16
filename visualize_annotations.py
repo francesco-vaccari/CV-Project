06c4104c3739596efb5b1737a9cd7aba410fffbf
francesco-vaccari <francesco.vaccari@studenti.unitn.it>
2024-08-16 18:21:37 +0200

    .

--- visualize_annotations.py
+++ visualize_annotations.py
@@ -1,15 +1,18 @@
 import cv2
 
 video = 'videos/refined2_short.mp4'
-annotations_file = 'annotations_1_4.txt'
+annotations_files = ['annotations_1_1.txt', 'annotations_1_2.txt', 'annotations_1_3.txt', 'annotations_1_4.txt', 'annotations_1_5.txt', 'annotations_1_6.txt']
 
 cap = cv2.VideoCapture(video)
 
 annotations = []
-with open(annotations_file, 'r') as file:
-    for line in file:
-        frame, x1, y1, x2, y2 = line.strip().split(',')
-        annotations.append((int(frame), int(x1), int(y1), int(x2), int(y2)))
+for file in annotations_files:
+    with open(file, 'r') as file:
+        player = []
+        for line in file:
+            frame, x1, y1, x2, y2 = line.strip().split(',')
+            player.append((int(frame), int(x1), int(y1), int(x2), int(y2)))
+        annotations.append(player)
 
 cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
 counter = 0
@@ -19,7 +22,8 @@ while cap.isOpened():
     if not ret:
         break
     
-    cv2.rectangle(frame, (annotations[counter][1], annotations[counter][2]), (annotations[counter][3], annotations[counter][4]), (0, 255, 0), 4)
+    for player in annotations:
+        cv2.rectangle(frame, (player[counter][1], player[counter][2]), (player[counter][3], player[counter][4]), (0, 255, 0), 4)
 
     cv2.imshow('frame', frame)
 
