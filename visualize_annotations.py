a4a9e7c06e4b9fddc4ffc27739f8440ab8907ed4
Michele Minniti <77793527+Sminnitex@users.noreply.github.com>
2024-08-21 14:26:48 +0200

    Adjusted to visualize ball data

--- visualize_annotations.py
+++ visualize_annotations.py
@@ -4,8 +4,6 @@ import os
 video = 'videos/refined2_short.mp4'
 annotations_folder = 'annotations'
 
-
-
 annotations_files = [os.path.join(annotations_folder, file) for file in os.listdir(annotations_folder) if file.endswith('.label')]
 
 cap = cv2.VideoCapture(video)
@@ -13,11 +11,16 @@ cap = cv2.VideoCapture(video)
 annotations = []
 for file in annotations_files:
     with open(file, 'r') as file:
-        player = []
+        entity_annotations = []
         for line in file:
-            frame, x1, y1, x2, y2 = line.strip().split(',')
-            player.append((int(frame), int(x1), int(y1), int(x2), int(y2)))
-        annotations.append(player)
+            values = list(map(int, line.strip().split(',')))
+            if len(values) == 5:  #Rectangle annotation (player)
+                frame, x1, y1, x2, y2 = values
+                entity_annotations.append((frame, "rectangle", x1, y1, x2, y2))
+            elif len(values) == 4:  #Circle annotation (ball)
+                frame, centerx, centery, radius = values
+                entity_annotations.append((frame, "circle", centerx, centery, radius))
+        annotations.append(entity_annotations)
 
 cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
 counter = 0
@@ -27,8 +30,14 @@ while cap.isOpened():
     if not ret:
         break
     
-    for player in annotations:
-        cv2.rectangle(frame, (player[counter][1], player[counter][2]), (player[counter][3], player[counter][4]), (0, 255, 0), 4)
+    for entity in annotations:
+        if counter < len(entity):
+            if entity[counter][1] == "rectangle":
+                _, _, x1, y1, x2, y2 = entity[counter]
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 4)
+            elif entity[counter][1] == "circle":
+                _, _, centerx, centery, radius = entity[counter]
+                cv2.circle(frame, (centerx, centery), radius, (0, 0, 255), 4)
 
     cv2.imshow('frame', frame)
 
