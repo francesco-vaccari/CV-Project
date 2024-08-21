e9c2ab609360e3be8bafd852dadea8b66b634433
Michele Minniti <77793527+Sminnitex@users.noreply.github.com>
2024-08-21 14:19:58 +0200

    Modified for writing ball coordinates

--- adjust_annotations.py
+++ adjust_annotations.py
@@ -4,8 +4,6 @@ import cv2
 annotations_folder = 'annotations'
 reference_video = 'videos/refined2_short.mp4'
 
-
-
 cap = cv2.VideoCapture(reference_video)
 height, width = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 
@@ -15,13 +13,23 @@ for file_path in annotations_files:
     out = []
     with open(file_path, 'r') as file:
         for line in file:
-            frame, x1, y1, x2, y2 = line.strip().split(',')
-            frame, x1, y1, x2, y2 = int(frame), int(x1), int(y1), int(x2), int(y2)
-            x1 = max(0, x1)
-            y1 = max(0, y1)
-            x2 = min(width, x2)
-            y2 = min(height, y2)
-            out.append(f'{frame},{x1},{y1},{x2},{y2}')
+            values = line.strip().split(',')
+            
+            if len(values) == 5:  #Rectangle annotation
+                frame, x1, y1, x2, y2 = map(int, values)
+                x1 = max(0, x1)
+                y1 = max(0, y1)
+                x2 = min(width, x2)
+                y2 = min(height, y2)
+                out.append(f'{frame},{x1},{y1},{x2},{y2}')
+            
+            elif len(values) == 4:  #Circle notation
+                frame, centerx, centery, radius = map(int, values)
+                centerx = max(0, min(width, centerx))
+                centery = max(0, min(height, centery))
+                radius = max(0, radius)  
+                out.append(f'{frame},{centerx},{centery},{radius}')
+
     out_path = file_path[:-4] + '.label'
     with open(out_path, 'w') as file:
         for line in out:
